1408403ad36ba14c9ead8693f92aca7e79ca55f9
稻草 <i@dcaoao.com>
2023-10-10 13:49:27 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -6,7 +6,15 @@ serve(async (request) => {
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
-    return fetch(new URL("./Readme.md", import.meta.url));
+    const readmeResponse = await fetch(new URL("./Readme.md", import.meta.url));
+    const readmeText = await readmeResponse.text();
+
+    // 返回编码为UTF-8的Readme.md内容
+    return new Response(readmeText, {
+      headers: {
+        "Content-Type": "text/plain; charset=UTF-8",
+      },
+    });
   }
 
   url.host = OPENAI_API_HOST;
